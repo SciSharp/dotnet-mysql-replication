a450b6708dcb2ac467171455fc6cf026f2fabc1d
Kerry Jiang <kerry-jiang@hotmail.com>
2019-10-09 18:35:58 -0700

    created the solution structure

---
+++ src/SciSharp.MySQL.Replication/ReplicationClient.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SciSharp.MySQL.Replication
+{
+    public class ReplicationClient
+    {
+        
+    }
+}
---
+++ tests/Test/MainTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Test
+{
+    public class MainTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
