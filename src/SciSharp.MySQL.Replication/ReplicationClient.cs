43345a5aa99f0e01a87b9a337e5d6ab082beb66d
Kerry Jiang <kerry-jiang@hotmail.com>
2019-10-10 23:51:55 -0700

    tried to implement the method ConnectAsync

--- src/SciSharp.MySQL.Replication/ReplicationClient.cs
+++ src/SciSharp.MySQL.Replication/ReplicationClient.cs
@@ -1,15 +1,63 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using System.Collections.Generic;
-
+using MySql.Data.MySqlClient;
+using System.Reflection;
 
 namespace SciSharp.MySQL.Replication
 {
     public class ReplicationClient : IReplicationClient
     {
-        public Task<LoginResult> ConnectAsync(string server, string username, string password)
+        private MySqlConnection _connection;
+
+        private Stream _stream;
+
+        private Stream GetStreamFromMySQLConnection(MySqlConnection connection)
         {
-            throw new NotImplementedException();
+            var driverField = connection.GetType().GetField("driver", BindingFlags.Instance | BindingFlags.NonPublic);
+            var driver = driverField.GetValue(connection);
+            var handlerField = driver.GetType().GetField("handler", BindingFlags.Instance | BindingFlags.NonPublic);
+            var handler = handlerField.GetValue(driver);
+            var baseStreamField = handler.GetType().GetField("baseStream", BindingFlags.Instance | BindingFlags.NonPublic);
+            return baseStreamField.GetValue(handler) as Stream;
+        }
+
+        public async Task<LoginResult> ConnectAsync(string server, string username, string password)
+        {
+            var connString = $"Server={server}; UID={username}; Password={password}";
+
+            var mysqlConn = new MySqlConnection(connString);
+
+            try
+            {
+                await mysqlConn.OpenAsync();
+            }
+            catch (Exception e)
+            {
+                return new LoginResult
+                {
+                    Result = false,
+                    Message = e.Message
+                };
+            }
+
+            try
+            {
+                _stream = GetStreamFromMySQLConnection(mysqlConn);
+                _connection = mysqlConn;
+                return new LoginResult { Result = true };
+            }
+            catch (Exception e)
+            {
+                await mysqlConn.CloseAsync();
+                
+                return new LoginResult
+                {
+                    Result = false,
+                    Message = e.Message
+                };
+            }
         }
 
         public IAsyncEnumerable<LogEvent> FetchEvents()
