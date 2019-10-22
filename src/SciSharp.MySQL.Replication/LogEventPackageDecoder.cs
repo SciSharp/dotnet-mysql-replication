a18615b04a01246662779d9d66ce4fc9dfa7f4de
Kerry Jiang <kerry-jiang@hotmail.com>
2019-10-22 00:39:14 -0700

    tried to implement LogEventPipelineFilter

---
+++ src/SciSharp.MySQL.Replication/LogEventPackageDecoder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Buffers;
+using SuperSocket.ProtoBase;
+
+namespace SciSharp.MySQL.Replication
+{
+    class LogEventPackageDecoder : IPackageDecoder<LogEvent>
+    {
+        public LogEvent Decode(ReadOnlySequence<byte> buffer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/SciSharp.MySQL.Replication/LogEventPipelineFilter.cs
+++ src/SciSharp.MySQL.Replication/LogEventPipelineFilter.cs
@@ -4,20 +4,29 @@ using SuperSocket.ProtoBase;
 
 namespace SciSharp.MySQL.Replication
 {
-    public class LogEventPipelineFilter : IPipelineFilter<LogEvent>
+    public class LogEventPipelineFilter : FixedHeaderPipelineFilter<LogEvent>
     {
-        public IPackageDecoder<LogEvent> Decoder { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-
-        public IPipelineFilter<LogEvent> NextFilter => throw new NotImplementedException();
-
-        public LogEvent Filter(ref SequenceReader<byte> reader)
+        public LogEventPipelineFilter()
+            : base(4)
         {
-            throw new NotImplementedException();
+            Decoder = new LogEventPackageDecoder();
         }
 
-        public void Reset()
+        protected override int GetBodyLengthFromHeader(ReadOnlySequence<byte> buffer)
         {
-            throw new NotImplementedException();
+            var pos = 0;
+            var len = 0;
+
+            foreach (var piece in buffer)
+            {
+                for (var i = 0; i < piece.Length && pos < 3; i++)
+                {
+                    len = len | ((0xff & piece.Span[i]) << (pos * 8));
+                    pos++;
+                }
+            }
+
+            return len;
         }
     }
 }
