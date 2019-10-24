d883859bc6321a24b2f88eeddab049166d5c5c4f
Kerry Jiang <kerry-jiang@hotmail.com>
2019-10-23 23:08:38 -0700

    added test cases

--- tests/Test/MainTest.cs
+++ tests/Test/MainTest.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 using SciSharp.MySQL.Replication;
 using Xunit;
 
@@ -17,9 +18,35 @@ namespace Test
         {
             var client = new ReplicationClient();
 
-            var result = await client.ConnectAsync("localhost", "root", "scisharp", 1, "mysql-bin.log");
+            var result = await client.ConnectAsync("localhost", "root", "scisharp", 1, "");
             
             Assert.True(result.Result, result.Message);
+
+            await client.CloseAsync();
+        }
+
+        [Fact]
+        public async Task TestReceiveEvent()
+        {
+            var client = new ReplicationClient();
+
+            var result = await client.ConnectAsync("localhost", "root", "scisharp", 1, "");
+            
+            Assert.True(result.Result, result.Message);
+
+            using (var mysqlConn = new MySqlConnection("Server=localhost;Database=garden;Uid=root;Pwd=scisharp;"))
+            {
+                await mysqlConn.OpenAsync();
+
+                var cmd = mysqlConn.CreateCommand();
+                cmd.CommandText = "INSERT INTO pet (name, owner, species, sex, birth, death) values ('Rokie', 'Kerry', 'abc', 'F', '1982-04-20', '3000-01-01')";
+                await cmd.ExecuteNonQueryAsync();
+
+                await foreach (var eventLog in client.FetchEvents())
+                {
+                    Assert.NotNull(eventLog);
+                }
+            }            
 
             await client.CloseAsync();
         }
