3abcc3562c7854d5475f91e19df3f969ef4e6a61
Kerry Jiang <kerry-jiang@hotmail.com>
2019-11-03 22:40:43 -0800

    added method ReadLengthEncodedInteger

--- src/SciSharp.MySQL.Replication/LogEvent.cs
+++ src/SciSharp.MySQL.Replication/LogEvent.cs
@@ -13,6 +13,35 @@ namespace SciSharp.MySQL.Replication
         public int Position { get; set; }
         public LogEventFlag Flags { get; set; }
         protected internal abstract void DecodeBody(ref SequenceReader<byte> reader);
+
+        protected long ReadLengthEncodedInteger(ref SequenceReader<byte> reader)
+        {
+            reader.TryRead(out byte b0);            
+
+            if (b0 == 0xFC)
+            {
+                reader.TryReadLittleEndian(out short shortValue);
+                return (long)shortValue;
+            }
+
+            if (b0 == 0xFD)
+            {
+                reader.TryRead(out byte b1);
+                reader.TryRead(out byte b2);
+                reader.TryRead(out byte b3);
+
+                return (long)(b1 + b2 * 256 + b3 * 256 * 256);
+            }
+
+            if (b0 == 0xFE)
+            {
+                reader.TryReadLittleEndian(out long longValue);
+                return longValue;
+            }
+
+            return (long)b0;
+        }
+
         public const int MARIA_SLAVE_CAPABILITY_GTID = 4;
         public const int MARIA_SLAVE_CAPABILITY_MINE = MARIA_SLAVE_CAPABILITY_GTID;
 
